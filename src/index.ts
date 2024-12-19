0da36be2f7d4262caaa36ca3d986b54409ea6bcc
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2024-12-19 17:59:36 +0900

    fix: tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { routes } from './routes'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+routes(app)
 
 const port = 3000
 console.log(`Server is running on http://localhost:${port}`)
